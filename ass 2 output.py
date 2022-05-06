4635303e9b304e57752ed91f0ea8a859cc878d8a
Pratham-sharma07 <102985188+Pratham-sharma07@users.noreply.github.com>
2022-05-07 00:04:33 +0530

    Add files via upload

---
+++ ass 2 output.py
@@ -0,0 +1,25 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==================== RESTART: C:/Users/acer/Desktop/ass2.py ====================
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive language
+Python is object oriented
+10
+Pythonisacasesensitivelanguage
+Hey,Pratham Sharma Here! My SID is 21107031,I am from Mechanical Engineering department and my CGPA is 9.9
+a&b: 8
+a|b: 58
+a^b 50
+a<<2: 224 	b<<2: 40
+a>>2: 14 tb>>2: 0
+Enter the string:hey
+No
+First side length :4
+Second side length :5
+Third side length:6
+4 5 6 Form a traingle
+Enter the 1st number:20
+Enter the 2nd number:30
+Required number of bits to be flipped: 2
